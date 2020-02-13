83b6f40e809c8bd0f5598064db1bf2d79417ed55
Davi Rodrigues <developer.davi@gmail.com>
2020-02-13 20:11:34 -0300

    Added custom style behaviour when pressed

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Styles from '../Styles'
 
-export default function Button({ text, color, style }) {
+export default function Button({ text, color, style, onPress }) {
+  const [beingPressed, setBeingPressed] = useState(false)
+
   return (
-    <TouchableOpacity style={[Styles.button, { backgroundColor: color }, style]}>
-      <Text style={Styles.buttonText}>{text}</Text>
+    <TouchableOpacity activeOpacity={1} onPress={onPress} onPressIn={() => setBeingPressed(true)} onPressOut={() => setBeingPressed(false)} style={[Styles.button, { borderWidth: 2, borderColor: color, backgroundColor: beingPressed ? '#ffffff00' : color }, style]}>
+      <Text style={[Styles.buttonText, { color: beingPressed ? color : Styles.buttonText.color }]}>{text}</Text>
     </TouchableOpacity>
   )
 }
