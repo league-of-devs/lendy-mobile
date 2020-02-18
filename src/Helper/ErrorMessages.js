2f99793b86f6c64d0635edba6bd7ef116185f329
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 19:45:28 -0300

    Added default error messages dictionary

---
+++ src/Helper/ErrorMessages.js
@@ -0,0 +1,19 @@
+const ErrorMsgs = {
+  invalid_name: 'Nome inválido!\nPor favor, verifique-o e tente novamente.',
+  long_name: 'Nome muito longo!\nPor favor, abrevie seu nome.',
+  short_name: 'Nome muito curto!\nPor favor, insira seu nome completo.',
+  invalid_email: 'E-mail inválido!\nPor favor, verifique-o e tente novamente.',
+  invalid_cpf: 'CPF inválido!\nPor favor, verifique-o e tente novamente.',
+  short_password: 'Senha muito curta!\nPor favor, insira uma senha maior.',
+  long_password: 'Senha muito longa!\nPor favor, insira uma senha menor.',
+  invalid_cep: 'CEP inválido!\nPor favor, verifique-o e tente novamente.',
+  invalid_address_number: 'Número de endereço inválido!\nPor favor, verifique-o e tente novamente.',
+  invalid_complement: 'Complemento de endereço inválido!\nPor favor, verifique-o e tente novamente.',
+  email_in_use: 'Este e-mail já está em uso!\nPor favor, insira outro e tente novamente.',
+  cpf_in_use: 'Este CPF já está em uso!\nÉ permitido ter apenas uma conta por pessoa.',
+  account_disabled: 'A sua conta está desativada!',
+  cant_login: 'Erro ao entrar!',
+  wrong_password: 'Senha incorreta!\nPor favor, verifique-a e tente novamente.'
+}
+
+export default ErrorMsgs
