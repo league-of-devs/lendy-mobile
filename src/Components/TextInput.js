c79a82aefb4d7d8fec1ad71c013c84e4d0ef5a0d
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 19:45:16 -0300

    Removed default value String()

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { TextInput as Input, StyleSheet, ViewPropTypes } from 'react-native'
+import { TextInput as Input } from 'react-native'
 import Styles from '../Styles'
 
-export default function TextInput({value = String(), onChange = (event) => { return event }, placeholder = String(), borderColor = String(), color = String(), placeholderColor = String(), style, ...props}) {
+export default function TextInput({value, onChange = (event) => { return event }, placeholder = String(), borderColor = String(), color = String(), placeholderColor = String(), style, ...props}) {
   return (
     <Input value={value} onChangeText={onChange} placeholder={placeholder} {...props} autoCompleteType='off' autoCorrect={false} placeholderTextColor={placeholderColor ? placeholderColor : '#ffffffee'} style={{ ...Styles.textInput, borderColor: borderColor || Styles.textInput.borderColor, color: color || Styles.textInput.color, ...style }}/>
   )
