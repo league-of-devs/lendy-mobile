e685b5bb48634bdcfaec81f9c670b904c1658d10
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 19:44:06 -0300

    Changed styling

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { Text } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Styles from '../Styles'
+import Colors from '../Styles/colors'
 
-export default function Button({ text, color, style, onPress }) {
+export default function Button({ text = String(), color = Colors.cyan, style = { }, onPress, textColor }) {
   const [beingPressed, setBeingPressed] = useState(false)
 
   return (
-    <TouchableOpacity activeOpacity={1} onPress={onPress} onPressIn={() => setBeingPressed(true)} onPressOut={() => setBeingPressed(false)} style={[Styles.button, { borderWidth: 2, borderColor: color, backgroundColor: beingPressed ? '#ffffff00' : color }, style]}>
-      <Text style={[Styles.buttonText, { color: beingPressed ? color : Styles.buttonText.color }]}>{text}</Text>
+    <TouchableOpacity activeOpacity={1} onPress={onPress} onPressIn={() => setBeingPressed(true)} onPressOut={() => setBeingPressed(false)} style={[Styles.button, { borderWidth: 2, borderColor: color, backgroundColor: beingPressed ? '#ffffff00' : color, elevation: beingPressed ? 0 : Styles.button.elevation }, style]}>
+      <Text style={[Styles.buttonText, { color: beingPressed ? color : textColor ? textColor : Styles.buttonText.color }]}>{text}</Text>
     </TouchableOpacity>
   )
 }
