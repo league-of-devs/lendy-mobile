9caed51bd71cb9a223cb580b92336c2cbcf1ed86
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 23:32:06 -0300

    Addeed redux state logic

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
-import { View, KeyboardAvoidingView, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, KeyboardAvoidingView, Text, Alert, AsyncStorage } from 'react-native'
 import Styles from '../Styles'
 import Logo from '../Components/Logo'
 import TextInput from '../Components/TextInput'
 import Button from '../Components/Button'
+import getAPI from '../Services/api'
+import ErrorMsgs from '../Helper/ErrorMessages'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Login({ navigation, ...props }) {
 
@@ -15,22 +18,64 @@ export default function Login({ navigation, ...props }) {
     setFormData(x)
   }
 
+  const userState = useSelector(state => state.user)
+  const dispatch = useDispatch()
+
+  const changeUserData = (data) => {
+    dispatch({ type: 'change_user_data', user: data })
+  } 
+
   const handleSubmit = () => {
-    navigation.navigate('Main')
+    getAPI().post('/user/login', formData)
+      .then(response => response.data)
+      .then(response => {
+        const { token } = response
+
+        AsyncStorage.setItem('@lendy/auth_token', token)
+          .then(() => {
+            changeUserData({ ...userState, token })
+            getAPI().get('/user/my_info', { headers: { authorization: token } })
+              .then(response => response.data)
+              .then(response => {
+                changeUserData({ token, ...response.data })
+              })
+              .then(() => navigation.navigate('Main'))
+          })
+      })
+      .catch((err) => {
+        Alert.alert('Erro', ErrorMsgs[err.response.data.error])
+      })
   }
+
+  useEffect(() => {
+    AsyncStorage.getItem('@lendy/auth_token')
+      .then(token => {
+        if (token) {
+          changeUserData({ token })
+
+          getAPI().get('/user/my_info', { headers: { authorization: token } })
+            .then(response => response.data)
+            .then(response => {
+              changeUserData({ token, ...response.data })
+            })
+            .then(() => navigation.navigate('Main'))
+        }
+      })
+
+  }, [])
 
   return (
     <View style={[Styles.container, Styles.mainBackground]}>
       <View style={[Styles.loginForm, { flex: 1, justifyContent: 'center' }]}>
         <KeyboardAvoidingView  style={{ flex: 0.7, justifyContent: 'flex-end' }} behavior='position'>
           <Logo white style={{width: '100%', transform: [ { scale: 0.7 } ]}}/>
-          <TextInput textContentType='emailAddress' autoCapitalize='none' onChange={(v) => handleChange('email', v)} placeholder='E-mail' keyboardType='email-address' style={{width: '100%'}}/>
+          <TextInput autoCapitalize='none' onChange={(v) => handleChange('cpf', v)} placeholder='CPF' keyboardType='numeric' style={{width: '100%'}}/>
           <TextInput onChange={(v) => handleChange('password', v)} placeholder='Senha' secureTextEntry={true} style={{width: '100%'}}/>
           <Button style={{ width: '50%' }} text='Log-in' color='#FFFFFFEE' onPress={handleSubmit} />
         </KeyboardAvoidingView>
         <View style={[{ flex: 0.3, justifyContent: 'center', alignContent: 'center' }]}>
           <Text style={Styles.text}>Ainda não é cadastrado?</Text>
-          <Text onPress={() => navigation.push('Signup')} style={[Styles.text, Styles.link]}>Cadastre-se</Text>
+          <Text onPress={() => navigation.navigate('Signup')} style={[Styles.text, Styles.link]}>Cadastre-se</Text>
         </View>
       </View>
     </View>
