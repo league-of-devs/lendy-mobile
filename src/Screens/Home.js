31676c693eb84d2ab23c6721f56acd84c0942e5a
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 23:31:26 -0300

    Added data from backend

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,40 +1,127 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useRef, useEffect } from 'react'
+import { View, Text, Animated, Easing } from 'react-native'
 import Styles from '../Styles'
 
-import { offers, requests } from '../fakeData.json'
-import Card from '../Components/Card'
 import { ScrollView } from 'react-native-gesture-handler'
-import Tag from '../Components/Tag'
-import TextInput from '../Components/TextInput'
-import Colors from '../Styles/colors'
 
-export default function Home() {
+import Switch from '../Components/Switch'
+import Header from '../Components/Header'
+import ItemCard from '../Components/ItemCard'
+import Button from '../Components/Button'
+import getAPI from '../Services/api'
+import { useSelector } from 'react-redux'
+
+export default function Home({ navigation }) {
+
+  const [switchValue, setSwitchValue] = useState(false)
+
+  const userState = useSelector(state => state.user)
+
+  const [offers, setOffers] = useState([])
+  const [myOffers, setMyOffers] = useState([])
+  const [requests, setRequests] = useState([])
+
+  const scrollPosition = new Animated.Value(0)
+  const scrollPositionRef = useRef(scrollPosition)
+
+  const handleScroll = (e) => {
+    const position = e.nativeEvent.contentOffset.y
+
+    Animated.timing(scrollPositionRef.current, {
+      toValue: position,
+      duration: 0,
+      easing: Easing.ease
+    }).start()
+  }
+
+  const getData = () => {
+    console.log(userState)
+
+    getAPI().get('/request/my_requests')
+      .then(response => response.data)
+      .then(response => {
+        setRequests(response.list)
+        console.log('MY REQUESTS', response.list)
+        // if (response.list.length > 0)
+        //   getAPI().get('/offer/search', { body: { minValue: response.list[0].value, maxFee: response.list[0].fee, minDays: response.list[0].days } })
+        //     .then(response => response.data)
+        //     .then(response => {
+        //       setOffers(response.data)
+        //     })
+      })
+
+    getAPI().get('/offer/my_offers')
+      .then(response => response.data)
+      .then(response => {
+        setMyOffers(response.list)
+      })
+
+  }
+
+  const opacityValue = scrollPositionRef.current.interpolate({
+    inputRange: [0, 40],
+    outputRange: [1, 0]
+  })
+  
+  useEffect(getData, [])
 
   return (
-    <View style={[Styles.container, { paddingTop: Styles.container.paddingTop + 15 }]}>
-      <View style={{ paddingHorizontal: 10 }}>
-        <TextInput borderColor={Colors.cyan} placeholder='Pesquisar por ofertas...' placeholderColor='#cccccc' color='#000' style={{ backgroundColor: '#eee', elevation: 5 }}/>
-      </View>
-      <ScrollView>
-        {offers.map((offer, i) => (
-          <Card key={i}>
-            <Text style={Styles.cardTitle}>R$ {Number.parseInt(offer.min_value)} - {Number.parseInt(offer.max_value)}</Text>
+    <View style={Styles.cyanContainer}>
+      <Header/>
+      <Animated.View style={{ paddingHorizontal: 10 }}>
+        <Switch options={ [ 'Investir', 'Solicitar' ] } onChange={setSwitchValue} />
+      </Animated.View>
+      <ScrollView onScroll={handleScroll}>
+        <Animated.View style={{ paddingHorizontal: 10, paddingVertical: 20, opacity: opacityValue }}>
+          { !switchValue ? (
             <View>
-              <Text style={[Styles.text]}>
-                {offer.from.name}
-                {' - '}
-                <Text style={[Styles.lightText]}>{offer.from.addr_city}, {offer.from.addr_state}</Text>
+              <Text style={[Styles.text, Styles.white, Styles.big]}>
+                <Text style={Styles.semiBold}>
+                  {myOffers.length > 0 ? `Belos investimentos, ${userState.name.split(' ')[0]}` : `Bora investir, ${userState.name.split(' ')[0]}?`}
+                </Text>
               </Text>
-              <Text numberOfLines={3} style={Styles.lightText}>{offer.description}</Text>
-              <View style={Styles.tagsContainer}>
-                {offer.tags.map((text, i) => (
-                  <Tag text={text} key={i}/>
-                ))}
-              </View>
+              <Text style={[Styles.text, Styles.white, Styles.big]}>
+                Você tem{ ' ' }
+                <Text style={Styles.semiBold}>
+                  { myOffers.length } ofertas{ ' ' }
+                </Text>
+                em aberto{ myOffers.length > 0 ? ':' : '.' } 
+              </Text>
             </View>
-          </Card>
-        ))}
+          ) : (
+            <View>
+              <Text style={[Styles.text, Styles.white, Styles.big]}>
+                <Text style={Styles.semiBold}>
+                  {myOffers.length > 0 ? `Boas notícias, ${userState.name.split(' ')[0]}!` : `Sinto muito, ${userState.name.split(' ')[0]}...`}
+                </Text>
+              </Text>
+              <Text style={[Styles.text, Styles.white, Styles.big]}>
+                Você possui{ ' ' }
+                <Text style={Styles.semiBold}>
+                  { offers.length } ofertas{ ' ' }
+                </Text>
+                disponíveis{ offers.length > 0 ? ':' : '.' } 
+              </Text>
+            </View>
+          ) }
+        </Animated.View>
+        { !switchValue ? 
+          myOffers.map((offer, i) => (
+            <ItemCard data={offer} key={i} type='offer' />
+          ))
+          : 
+          offers.map((request, i) => (
+            <ItemCard data={request} key={i} type='request' />
+          ))
+        }
+        <View style={Styles.padding}>
+
+          { !switchValue ? 
+            <Button onPress={() => navigation.navigate('CreateItem', { type: 'offer' })} textColor='#707070' color='#fff' text='Adicionar oferta'/>
+            :
+            <Button onPress={() => navigation.navigate('CreateItem', { type: 'request' })} textColor='#707070' color='#fff' text='Criar solicitação'/>
+          }
+        </View>
       </ScrollView>
     </View>
   )
