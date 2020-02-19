f493edd14a46f8d33a529baf3d0f1f2b1af0984d
Davi Rodrigues <developer.davi@gmail.com>
2020-02-18 23:30:13 -0300

    Changed placeholder color

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -4,6 +4,6 @@ import Styles from '../Styles'
 
 export default function TextInput({value, onChange = (event) => { return event }, placeholder = String(), borderColor = String(), color = String(), placeholderColor = String(), style, ...props}) {
   return (
-    <Input value={value} onChangeText={onChange} placeholder={placeholder} {...props} autoCompleteType='off' autoCorrect={false} placeholderTextColor={placeholderColor ? placeholderColor : '#ffffffee'} style={{ ...Styles.textInput, borderColor: borderColor || Styles.textInput.borderColor, color: color || Styles.textInput.color, ...style }}/>
+    <Input value={value} onChangeText={onChange} placeholder={placeholder} {...props} autoCompleteType='off' autoCorrect={false} placeholderTextColor={placeholderColor ? placeholderColor : '#ffffffcc'} style={{ ...Styles.textInput, borderColor: borderColor || Styles.textInput.borderColor, color: color || Styles.textInput.color, ...style }}/>
   )
 }
